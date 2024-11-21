aa95f016d9a0474972ac840c0dbbf7c2cc38ec39
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-11-21 17:24:10 -0300

    Inital commit project espaco dialogico

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <div>Site em construção, acesse o nosso perfil no instagram para saber mais <a href="https://www.instagram.com/espacodialogico/">Espaço Dialógico</a></div>
+}
+
+export default Home;
