fbda404c55355c72427ec6e0a489f95e5f67c6bf
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-11-21 18:11:37 -0300

    fix: adapt pages estructures for site and sys

---
+++ pages/site/index.js
@@ -0,0 +1,9 @@
+function SitePage() {
+  return (
+    <div>
+      Site em construção, acesse o nosso perfil no instagram para saber mais <a href="https://www.instagram.com/espacodialogico/">Espaço Dialógico</a>
+    </div>
+  );
+}
+
+export default SitePage;
