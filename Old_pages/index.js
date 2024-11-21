24ff7b5960012481f728d7ac28552c66656fbe35
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-11-21 17:46:47 -0300

    Initial commit

--- Old_pages/index.js
+++
@@ -1,5 +0,0 @@
-function Home() {
-  return <div>Site em construção, acesse o nosso perfil no instagram para saber mais <a href="https://www.instagram.com/espacodialogico/">Espaço Dialógico</a></div>
-}
-
-export default Home;
