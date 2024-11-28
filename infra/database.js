322d9f2e8e3bf7677b39f4b50b4d6f6bb1536ca5
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-11-28 14:05:58 -0300

    makes `database.js` client.connect()  more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,26 +9,24 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD
   });
 
-  // console.log('Credenciais do Postgres:', {
-  //   host: process.env.POSTGRES_HOST,
-  //   port: process.env.POSTGRES_PORT,
-  //   user: process.env.POSTGRES_USER,
-  //   database: process.env.POSTGRES_DB,
-  //   password: process.env.POSTGRES_PASSWORD
-  // });
+  console.log('Credenciais do Postgres:', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
 
-
-  await client.connect();
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
-
   } catch (error) {
     console.error('Erro ao executar query:', error);
+    throw error;
   } finally {
     await client.end();
-
   }
 }
 
