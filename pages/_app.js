e9bba1a2e938c0f5ca5ab88381aa1e775b94c03e
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-12-11 11:03:11 -0300

    fix `eslint` linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa o CSS do Bootstrap
-import 'bootstrap-icons/font/bootstrap-icons.css'; // Importa os ícones do Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap
+import "bootstrap-icons/font/bootstrap-icons.css"; // Importa os ícones do Bootstrap
 // import '../style/estilo.css'; // Importa seu CSS customizado
 // import '../styles/globals.css'; // Opcional: se você tiver outros estilos globais
 
