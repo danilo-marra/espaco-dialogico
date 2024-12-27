b42d388d7087e0c23293dea35d9559222d5e83d3
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-12-27 12:05:50 -0300

    test: fix tests

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -3,41 +3,42 @@ import database from "infra/database.js";
 
 async function waitForAllServices() {
   await waitForWebServer();
-  await waitForTerapeutasEndpoint();
-}
+  await waitForDatabase();
 
-async function waitForWebServer() {
-  return retry(fetchStatusPage, {
-    retries: 100,
-    maxTimeout: 1000,
-  });
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-  async function fetchStatusPage() {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
 
-    if (response.status !== 200) {
-      throw Error("Status endpoint not ready");
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
-}
 
-async function waitForTerapeutasEndpoint() {
-  return retry(fetchTerapeutasPage, {
-    retries: 100,
-    maxTimeout: 1000,
-  });
+  async function waitForDatabase() {
+    return retry(checkDatabaseConnection, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
 
-  async function fetchTerapeutasPage() {
-    const response = await fetch("http://localhost:3000/api/v1/terapeutas");
-
-    if (response.status !== 200) {
-      throw Error("Terapeutas endpoint not ready");
+    async function checkDatabaseConnection() {
+      try {
+        const client = await database.getNewClient();
+        await client.end();
+      } catch (error) {
+        throw Error();
+      }
     }
   }
 }
 
 async function clearDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+  await database.query("drop schema public cascade; create schema public;");
 }
 
 const orchestrator = {
