7c6bfc8e12cccf2a424cbb35c5be8b97688e4c04
Danilo Marra Rabelo <danilo2311@gmail.com>
2025-02-17 14:21:30 -0300

    fix: add `migrator` to api terapeutas endpoint

--- pages/api/v1/terapeutas/index.js
+++ pages/api/v1/terapeutas/index.js
@@ -1,6 +1,7 @@
 import { createRouter } from "next-connect";
 import database from "infra/database.js";
 import controller from "infra/controller";
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -9,6 +10,8 @@ router.get(getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
+  await migrator.runPendingMigrations();
+
   const terapeutas = await database.query("SELECT * FROM terapeutas;");
   res.status(200).json(terapeutas.rows);
 }
