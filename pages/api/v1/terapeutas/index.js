f5e1b3f1497cab7ada929a9dad1555ee38388b69
Danilo Marra Rabelo <danilo2311@gmail.com>
2025-02-17 14:31:51 -0300

    fix: remove migrator

--- pages/api/v1/terapeutas/index.js
+++ pages/api/v1/terapeutas/index.js
@@ -1,7 +1,6 @@
 import { createRouter } from "next-connect";
 import database from "infra/database.js";
 import controller from "infra/controller";
-import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -10,8 +9,6 @@ router.get(getHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(req, res) {
-  await migrator.runPendingMigrations();
-
   const terapeutas = await database.query("SELECT * FROM terapeutas;");
   res.status(200).json(terapeutas.rows);
 }
