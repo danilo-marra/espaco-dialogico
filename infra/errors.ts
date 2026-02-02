4ea51eca0fe182d6eff83dee9365f1104ba1195a
Danilo Marra Rabelo <danilo2311@gmail.com>
2026-02-02 10:09:13 -0300

    fix: fix error on tests

--- infra/errors.ts
+++ infra/errors.ts
@@ -39,6 +39,56 @@ export class ValidationError extends BaseError {
   }
 }
 
+export class NotFoundError extends BaseError {
+  constructor({ message, action }: { message?: string; action?: string } = {}) {
+    super({
+      message: message || "Não foi possível encontrar este recurso no sistema.",
+      action:
+        action ||
+        "Verifique se os parâmetros enviados na consulta estão certos.",
+      statusCode: 404,
+    });
+  }
+}
+
+export class MethodNotAllowedError extends BaseError {
+  constructor() {
+    super({
+      message: "Método não permitido para este endpoint.",
+      action: "Verifique se o método HTTP enviado é válido para este endpoint.",
+      statusCode: 405,
+    });
+  }
+}
+
+export class InternalServerError extends BaseError {
+  constructor({ message, cause }: { message?: string; cause?: unknown } = {}) {
+    super({
+      message: message || "Um erro interno não esperado aconteceu.",
+      action: "Entre em contato com o suporte.",
+      statusCode: 500,
+    });
+
+    if (cause) {
+      (this as { cause?: unknown }).cause = cause;
+    }
+  }
+}
+
+export class ServiceError extends BaseError {
+  constructor({ message, cause }: { message?: string; cause?: unknown } = {}) {
+    super({
+      message: message || "Serviço indisponível no momento.",
+      action: "Verifique se o serviço está disponível.",
+      statusCode: 503,
+    });
+
+    if (cause) {
+      (this as { cause?: unknown }).cause = cause;
+    }
+  }
+}
+
 export class TerapeutaError extends ValidationError {
   constructor(message: string) {
     super({
