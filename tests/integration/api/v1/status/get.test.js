36d189c1673ebd47ba51af38654c66fe77fdbf59
Danilo Marra Rabelo <danilo2311@gmail.com>
2026-06-01 15:28:51 -0300

    test: ajustar status para pool de conexões

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -20,7 +20,12 @@ describe("GET /api/v1/status", () => {
 
       expect(responseBody.dependencies.database.version).toEqual("16.0");
       expect(responseBody.dependencies.database.max_connections).toEqual(100);
-      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeGreaterThanOrEqual(1);
+      expect(
+        responseBody.dependencies.database.opened_connections,
+      ).toBeLessThanOrEqual(responseBody.dependencies.database.max_connections);
     });
   });
 });
